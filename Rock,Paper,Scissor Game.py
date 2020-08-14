126c3e5eb2fa30fb420f5e067ef793d1bd5cc4f9
NITIN SHARMA <64805406+sharmanitin1507@users.noreply.github.com>
2020-08-14 06:47:47 +0530

    Add files via upload

---
+++ Rock,Paper,Scissor Game.py
@@ -0,0 +1,41 @@
+from random import randint
+print("you are welcome in this game")
+
+#create a list of play options
+t = ["Rock", "Paper", "Scissors"]
+
+#assign a random play to the computer
+computer = t[randint(0,2)]
+
+#set player to False
+player = False
+
+while player == False:
+#set player to True
+    player = input("Your choice : Rock, Paper, Scissors?")
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+        else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+    #player was set to True, but we want it to be False so the loop continues
+    a=input("Want to play again y/n?")
+    if (a=="y" or a=="Y"):
+        player = False
+        computer = t[randint(0,2)]
+    else:
+        print("Thankyou for playing")
